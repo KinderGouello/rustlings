581cbe2ed6257319268c886ed4767cc3ff351aae
Remy Gouello <r.gouello-ext@kreactive.com>
2024-06-15 19:44:55 +0200

    do exercices

--- exercises/08_enums/enums3.rs
+++ exercises/08_enums/enums3.rs
@@ -5,10 +5,12 @@
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    Move(Point),
+    ChangeColor((u8, u8, u8)),
+    Echo(String),
+    Quit,
 }
 
 struct Point {
@@ -42,8 +44,12 @@ impl State {
 
     fn process(&mut self, message: Message) {
         // TODO: create a match expression to process the different message variants
-        // Remember: When passing a tuple as a function argument, you'll need extra parentheses:
-        // fn function((t, u, p, l, e))
+        match message {
+            Message::Move(point) => self.move_position(point),
+            Message::Echo(string) => self.echo(string),
+            Message::ChangeColor(color) => self.change_color(color),
+            Message::Quit => self.quit(),
+        }
     }
 }
 
